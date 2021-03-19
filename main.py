36fd1ca0c6e7c6621d15dc0bc7abc75d92a90022
Antoine Savard <antoine.savard@mail.mcgill.ca>
2021-03-19 14:56:01 -0400

    datatype change

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ dataset = vis.Arctic(
     directory="spin_up10",
     time="1986-01-01-00-00",
     expno="03",
-    datatype="u",
+    datatype="dedt",
     fig_shape="round",
     save=1,
 )
 
 print("\nWelcome in the data loader routine! To cancel selection, enter exit or quit.")
 
-# dataset.arctic_plot(dataset.load())
+dataset.arctic_plot(dataset.load())
 # dataset.multi_load("01-00-00", "1997-03-31-00-00")
 
-L = [80, 160, 320, 640]
-dt = "01-00-00"
-time_end = "1997-03-31-00-00"
-data_box = dataset.spatial_mean_box(dataset.multi_load(dt, time_end), L, dt)
-dataset.scale_plot(data_box, L)
-plt.show()
+# L = [80, 160, 320, 640]
+# dt = "01-00-00"
+# time_end = "1997-03-31-00-00"
+# data_box = dataset.spatial_mean_box(dataset.multi_load(dt, time_end), L, dt)
+# dataset.scale_plot(data_box, L)
+# plt.show()
