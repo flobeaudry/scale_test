63742c5d0d9568c59e495bfe08e06c6020b19d3e
Antoine Savard <antoine.savard@mail.mcgill.ca>
2021-09-03 13:40:11 -0400

    constants file

---
+++ libs/constants.py
@@ -0,0 +1,9 @@
+R_EARTH = 6370  # Earth's radius (is smaller for better looking plots)
+BETA = 32  # Angle between domain and Greenwich
+E = 2  # ellipse ratio
+ETA_MAX = 1e12  # max viscosity
+P_STAR = 27500  # chosen value for the ice strenght parameter in N/m^2
+C = 20  # arbitrary parameter
+BETA_RGPS = 45  # Angle between domain and Greenwich
+RES_RGPS = 12.5  # rgps resolution
+PLANE_RGPS = 70  # rgps sterographic plane latitude
