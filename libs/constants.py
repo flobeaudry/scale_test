9b57c6dd24836e7d9660ff3c85b7a98bbb8dc08f
Antoine Savard <antoine.savard@mail.mcgill.ca>
2022-05-04 17:33:21 -0400

    switch constants to floats

--- libs/constants.py
+++ libs/constants.py
@@ -1,9 +1,9 @@
-R_EARTH = 6370  # Earth's radius (is smaller for better looking plots)
-BETA = 32  # Angle between domain and Greenwich
-E = 2  # ellipse ratio
+R_EARTH = 6370.0  # Earth's radius (is smaller for better looking plots)
+BETA = 32.0  # Angle between domain and Greenwich
+E = 2.0  # ellipse ratio
 ETA_MAX = 1e12  # max viscosity
-P_STAR = 27500  # chosen value for the ice strenght parameter in N/m^2
-C = 20  # arbitrary parameter
-BETA_RGPS = 45  # Angle between domain and Greenwich
+P_STAR = 27500.0  # chosen value for the ice strenght parameter in N/m^2
+C = 20.0  # arbitrary parameter
+BETA_RGPS = 45.0  # Angle between domain and Greenwich
 RES_RGPS = 12.5  # rgps resolution
-PLANE_RGPS = 70  # rgps sterographic plane latitude
+PLANE_RGPS = 70.0  # rgps sterographic plane latitude
